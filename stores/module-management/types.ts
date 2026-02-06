4b145361b907d8be1e3d9ac95678a452ba5cba37
Wendy Hu <yunhu412@gmail.com>
2026-02-06 13:50:29 +0800

    feat(module-management): move all types to types.ts

---
+++ stores/module-management/types.ts
@@ -0,0 +1,51 @@
+import type { LucideIcon } from "lucide-react";
+
+export type PopUpChildProps = {
+  action?: string;
+};
+
+export type ModuleType = {
+  name: string;
+  storeKey: "AiModule" | "CmsModule" | "CodeModule" | "ExportModule"; // 對應 Zustand Store 的 key
+  units: ModuleUnit[];
+};
+
+export type ModuleUnit = {
+  id: number;
+  action: string;
+  actionIcon: LucideIcon;
+  actionCode: "ai" | "code" | "cms" | "content";
+  description: string;
+  popUpChild: React.ComponentType<PopUpChildProps>;
+};
+
+export type BasicModuleData = {
+  id: string;
+  name: string;
+  description?: string;
+};
+
+export type AiModuleSlice = {
+  AiModules: BasicModuleData[];
+  addAiModule: (name: string, description?: string) => void;
+};
+
+export type CodeModuleSlice = {
+  CodeModules: BasicModuleData[];
+  addCodeModule: (name: string, description?: string) => void;
+};
+
+export type CmsModuleSlice = {
+  CmsModules: BasicModuleData[];
+  addCmsModule: (name: string, description?: string) => void;
+};
+
+export type ContentModuleSlice = {
+  ContentModules: BasicModuleData[];
+  addContentModule: (name: string, description?: string) => void;
+};
+
+export type ModuleStore = AiModuleSlice &
+  CodeModuleSlice &
+  CmsModuleSlice &
+  ContentModuleSlice;
